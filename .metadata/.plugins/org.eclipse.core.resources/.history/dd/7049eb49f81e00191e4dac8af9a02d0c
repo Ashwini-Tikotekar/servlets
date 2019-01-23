2ece8b9423e4ae704ea93ce6870ddcbf34b3a0c9
Ashwini-Tikotekar <ashwinitikotekar@gmail.com>
2019-01-23 19:12:16 +0530

    fisrt commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/dd/7049eb49f81e00191e4dac8af9a02d0c
@@ -0,0 +1,21 @@
+package com.bridgelabz.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LogoutServlet extends HttpServlet{
+	public void doGet(HttpServletRequest req,HttpServletResponse resp) throws IOException, ServletException {
+		resp.setContentType("logout.html");
+	
+		PrintWriter pw = resp.getWriter();
+		RequestDispatcher rd2 ;
+		rd2=req.getRequestDispatcher("/login.html");
+		rd2.forward(req, resp);
+}
+}
---
+++ ServletJDBCApp/src/model/UserDetails.java
@@ -0,0 +1,59 @@
+package model;
+
+public class UserDetails {
+	private	String username;
+	private	String Password ;
+	private String lastname;
+	private String email;
+	private long mobileno;
+	
+	 //constructor 
+	public UserDetails(){
+		username=null;
+		Password =null;
+		lastname=null;
+		email=null;
+		mobileno=0;
+		
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return Password;
+	}
+
+	public void setPassword(String password) {
+		Password = password;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public long getMobileno() {
+		return mobileno;
+	}
+
+	public void setMobileno(long mobileno) {
+		this.mobileno = mobileno;
+	}
+}	
